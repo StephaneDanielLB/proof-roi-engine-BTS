bdb5d5200f5a1e47da1b859a1d28c4022fd9e2d1
Stéphane Daniel LE BRETON <stephane@buytryshare.com>
2026-05-01 23:34:32 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 // src/config/modelConfig.js
 window.BTS_LOGO_SRC = "./bts-logo-inverted.png";
-window.PROOF_MARK_SRC = "/proof-roi-engine/proof-roi-engine.png";
+window.PROOF_MARK_SRC = "./proof-roi-engine.png";
 var mediaTypes = {
   "Daytime": {
     "label": "Daytime"
